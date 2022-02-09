9054283ffe1bc5891e05a6f266f40d4e8e8370bb
churchil15 <churchil.rajpal15@gmail.com>
2022-02-09 21:06:22 +0530

    added everything to the staging area

---
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.courtcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        displayForTeamA(15);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void Point_3A(View view){
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void Point_2A(View view){
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void Point_1A(View view){
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score){
+        TextView scoreView  = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void Point_3B(View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void Point_2B(View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void Point_1B(View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void reset(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+}
+
+// This is my method to display score from previous Just java app (alternate of displayForTeamA() method)--
+
+//    private void displayScore(int num){
+//        TextView score = (TextView) findViewById(R.id.team_a_score);
+//        score.setText("" + num);
+//    }
